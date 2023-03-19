f7e8b1151a479853e436f0e49f5e60dee8204257
Shafie <ashafie@banquemisr.com>
2023-03-19 19:42:17 +0200

    sec commit pc

--- app/src/main/java/com/example/webapp/MainActivity.kt
+++ app/src/main/java/com/example/webapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.webapp
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import com.example.webapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    lateinit var activitMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activitMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activitMainBinding.root)
     }
+/*    private fun loadFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }*/
 }
