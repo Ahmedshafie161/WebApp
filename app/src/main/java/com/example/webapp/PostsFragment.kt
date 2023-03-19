397710fcf9452d192c1c0faab3f0657a1ed9c1df
ahmed shafie <ahmed.elmenshawie@gmail.com>
2023-03-19 15:35:11 +0000

    first commit

--- app/src/main/java/com/example/webapp/PostsFragment.kt
+++ app/src/main/java/com/example/webapp/PostsFragment.kt
@@ -27,7 +27,6 @@ class PostsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
